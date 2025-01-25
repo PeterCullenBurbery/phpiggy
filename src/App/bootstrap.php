d867988a99618ceff457eeb9dc84a2a1ec3019bd
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-01-25 17:52:41 -0500

    making my first commit in a while

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
-require __DIR__ ."/../../vendor/autoload.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Config\Paths;
+use Dotenv\Dotenv;
+use function App\Config\{registerRoutes, registerMiddleware};
 
-$app=new App();
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
+$app = new App(Paths::SOURCE . "app/container-definitions.php");
+registerRoutes($app);
+registerMiddleware($app);
 return $app;
